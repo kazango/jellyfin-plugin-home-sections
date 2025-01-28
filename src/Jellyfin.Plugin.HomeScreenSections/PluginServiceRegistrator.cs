a39ddcfb87c27d26525e67257d58b1ae9bb226e4
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-01-28 02:18:59 +0000

    Updated to 1.0.5

--- src/Jellyfin.Plugin.HomeScreenSections/PluginServiceRegistrator.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/PluginServiceRegistrator.cs
@@ -9,39 +9,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.HomeScreenSections
 {
-    public class PluginServiceRegistrator : IPluginServiceRegistrator
+    public class PluginServiceRegistrator : Services.PluginServiceRegistrator
     {
-        public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+        public override void ConfigureServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
         {
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-            {
-                if (args.Name.StartsWith("Jellyfin.Plugin.FileTransformation"))
-                {
-                    Assembly? assembly = AssemblyLoadContext.All.Where(x => !x.IsCollectible).SelectMany(x => x.Assemblies)
-                        .FirstOrDefault(x => x.FullName == args.Name);
-                    // Assembly? assembly = AppDomain.CurrentDomain.GetAssemblies()
-                    //     .FirstOrDefault(x => x.FullName == args.Name);
-                    if (assembly != null)
-                    {
-                        return assembly;
-                    }
-                    
-                    string pluginsDir = Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-
-                    string fileTransformationDir = Directory.GetDirectories(pluginsDir, "File Transformation_*", SearchOption.TopDirectoryOnly).OrderBy(
-                        x =>
-                        {
-                            return Version.Parse(x.Split('_').LastOrDefault());
-                        }).Last();
-                    
-                    string dll = Path.Combine(fileTransformationDir, "Jellyfin.Plugin.FileTransformation.dll");
-                    
-                    return Assembly.LoadFile(dll);
-                }
-                
-                return null;
-            };
-            
             serviceCollection.AddSingleton<CollectionManagerProxy>();
             serviceCollection.AddSingleton<IHomeScreenManager, HomeScreenManager>(services =>
             {
