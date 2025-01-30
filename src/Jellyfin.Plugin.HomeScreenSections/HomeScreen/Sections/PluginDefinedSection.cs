94986749dddf8b8450ead38ac15844a59d3ddc90
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-01-30 23:02:56 +0000

    Added class for other plugins to define sections that appear on the home screen. Currently the inheritance approach doesn't work, but this solves an issue.

---
+++ src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/PluginDefinedSection.cs
@@ -0,0 +1,39 @@
+﻿using Jellyfin.Plugin.HomeScreenSections.Library;
+using Jellyfin.Plugin.HomeScreenSections.Model.Dto;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Model.Querying;
+
+namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections
+{
+    public class PluginDefinedSection : IHomeScreenSection
+    {
+        public delegate QueryResult<BaseItemDto> GetResultsDelegate(HomeScreenSectionPayload payload);
+        
+        public string? Section { get; }
+        public string? DisplayText { get; set; }
+        public int? Limit { get; }
+        public string? Route { get; }
+        public string? AdditionalData { get; set; }
+
+        public required GetResultsDelegate OnGetResults { get; set; }
+        
+        public PluginDefinedSection(string sectionUuid, string displayText, string? route = null, string? additionalData = null)
+        {
+            Section = sectionUuid;
+            DisplayText = displayText;
+            Limit = 1;
+            Route = route;
+            AdditionalData = additionalData;
+        }
+        
+        public QueryResult<BaseItemDto> GetResults(HomeScreenSectionPayload payload)
+        {
+            return OnGetResults(payload);
+        }
+
+        public IHomeScreenSection CreateInstance(Guid? userId, IEnumerable<IHomeScreenSection>? otherInstances = null)
+        {
+            return this;
+        }
+    }
+}
