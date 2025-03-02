d5861dde1d847c26a9274c34db7c5d9392372119
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-03-02 09:02:33 +0000

    Updated pipe path code to be platform agnostic.

--- src/Jellyfin.Plugin.HomeScreenSections/Services/StartupService.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/Services/StartupService.cs
@@ -78,9 +78,11 @@ namespace Jellyfin.Plugin.HomeScreenSections.Services
                     });
 
                     string fileTransformationPipeName = "Jellyfin.Plugin.FileTransformation.NamedPipe";
-                    bool serverSupportsPipeCommunication = Directory.GetFiles(@"\\.\pipe\").Contains($@"\\.\pipe\{fileTransformationPipeName}");
-
-                    if (serverSupportsPipeCommunication)
+                    MethodInfo? getPipePathMethod = typeof(PipeStream).GetMethod("GetPipePath", BindingFlags.Static | BindingFlags.NonPublic);
+                    string? pipePath = getPipePathMethod?.Invoke(null, new object[] { ".", fileTransformationPipeName }) as string;
+                    string? pipeDirectory = Path.GetDirectoryName(pipePath);
+            
+                    if (Directory.Exists(pipeDirectory) && Directory.GetFiles(pipeDirectory).Contains(pipePath))
                     {
                         NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", fileTransformationPipeName, PipeDirection.InOut);
                         await pipeClient.ConnectAsync();
