a00690f76e92da33112cf8de4aefbba8cc8ee151
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-03-26 21:51:53 +0000

    Fixed issue with HomeScreenSections.js using a duplicate js class name.

--- src/Jellyfin.Plugin.HomeScreenSections/Inject/HomeScreenSections.js
+++ src/Jellyfin.Plugin.HomeScreenSections/Inject/HomeScreenSections.js
@@ -1,31 +1,33 @@
 'use strict';
 
-const HomeScreenSections = {
-    init: function() {
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-        var myObserver = new MutationObserver(this.mutationHandler);
-        var observerConfig = { childList: true, characterData: true, attributes: true, subtree: true };
+if (typeof TopTenSectionHandler == 'undefined') {
+    const TopTenSectionHandler = {
+        init: function () {
+            var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+            var myObserver = new MutationObserver(this.mutationHandler);
+            var observerConfig = {childList: true, characterData: true, attributes: true, subtree: true};
 
-        $("body").each(function () {
-            myObserver.observe(this, observerConfig);
-        });
-    },
-    mutationHandler: function(mutationRecords) {
-        mutationRecords.forEach(function (mutation) {
-            if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-                [].some.call(mutation.addedNodes, function(addedNode) {
-                    if ($(addedNode).hasClass('card')) {
-                        if ($(addedNode).parents('.top-ten').length > 0) {
-                            var index = parseInt($(addedNode).attr('data-index'));
-                            $(addedNode).attr('data-number', index + 1);
+            $("body").each(function () {
+                myObserver.observe(this, observerConfig);
+            });
+        },
+        mutationHandler: function (mutationRecords) {
+            mutationRecords.forEach(function (mutation) {
+                if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+                    [].some.call(mutation.addedNodes, function (addedNode) {
+                        if ($(addedNode).hasClass('card')) {
+                            if ($(addedNode).parents('.top-ten').length > 0) {
+                                var index = parseInt($(addedNode).attr('data-index'));
+                                $(addedNode).attr('data-number', index + 1);
+                            }
                         }
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
+        }
     }
-}
 
-setTimeout(function () {
-    HomeScreenSections.init();
-}, 50);
+    setTimeout(function () {
+        TopTenSectionHandler.init();
+    }, 50);
+}
